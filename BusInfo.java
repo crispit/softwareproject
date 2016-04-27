5bc189cff1084443d169cb5b764c21bb9fa134c1
Felixeh <felixeh@student.chalmers.se>
2016-04-27 12:44:14 +0200

    Added files via upload

--- BusInfo.java
+++ BusInfo.java
@@ -2,12 +2,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.*;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class BusInfo {
 
-   public static String getBusInfo(String dgwNr, String infoTxt) throws IOException {
+   public static String[] getSensors(){
+      String[] sensors = new String[25];
+   
+      sensors[0]="Accelerator_Pedal_Position";
+      sensors[1]="Ambient_Temperature";
+      sensors[2]="At_Stop";
+      sensors[3]="Cooling_Air_Conditioning";
+      sensors[4]="Driver_Cabin_Temperature";
+      sensors[5]="Fms_Sw_Version_Supported";
+      sensors[6]="GPS";
+      sensors[7]="GPS2";
+      sensors[8]="GPS_NMEA";
+      sensors[9]="Journey_Info";
+      sensors[10]="Mobile_Network_Cell_Info";
+      sensors[11]="Mobile_Network_Signal_Strength";
+      sensors[12]="Next_Stop";
+      sensors[13]="Offroute";
+      sensors[14]="Online_Users";
+      sensors[15]="Opendoor";
+      sensors[16]="Position_Of_Doors";
+      sensors[17]="Pram_Request";
+      sensors[18]="Ramp_Wheel_Chair_Lift";
+      sensors[19]="Status_2_Of_Doors";
+      sensors[20]="Stop_Pressed";
+      sensors[21]="Stop_Request";
+      sensors[22]="Total_Vehicle_Distance";
+      sensors[23]="Turn_Signals";
+      sensors[24]="Wlan_Connectivity";
+   
+      return sensors;
+   }
+   
+
+
+
+   public static String getBusInfo(String dgwNr, String infoTxt) throws IOException, EmptyReturnValueExcaption {
       String dgw = "Ericsson$" + dgwNr;
     
       long t2 = System.currentTimeMillis();
@@ -30,8 +66,6 @@ public class BusInfo {
       BufferedReader in = new BufferedReader(new InputStreamReader(
          	con.getInputStream()));
             
-            System.out.println((new InputStreamReader(
-         	con.getInputStream())).read());
             
       String inputLine;
             
@@ -40,7 +74,63 @@ public class BusInfo {
          response.append(inputLine);
       }
       in.close();
-      return response.toString();
+      if (response.toString().equals("")){
+         throw new EmptyReturnValueExcaption();
+      }
+      return "Felix"; // response.toString();
+   
+   }
+   
+   private static class wThreads implements Runnable {
+   
+      private final String sensor;
+      private final String dgwNr;
+      private final HashMap<String,String> busDataMap;
+        
+      public wThreads(String dgwNr, String sensor,HashMap<String,String> busDataMap) {
+         this.sensor=sensor;
+         this.dgwNr=dgwNr;
+         this.busDataMap=busDataMap;
+      }
+      @Override
+        public void run() {
+         try{
+            busDataMap.put(sensor,getBusInfo(dgwNr,sensor));
+         }
+         catch (IOException e) {
+            busDataMap.put(sensor,"Error getting data");
+               
+         }
+         catch (EmptyReturnValueExcaption e) {
+            busDataMap.put(sensor,"Platform returned no value");
+         }
+      }
+   }
+   
+   public static HashMap<String,String> getAllBusInfo (String dgwNr){
+   
+      String[] sensors = getSensors();
+      HashMap<String, String> busDataMap = new HashMap<String, String>(32);
+   
+      Thread[] myThreads;
+      myThreads = new Thread[sensors.length];
+   
+      for (int i = 0; i < sensors.length; i++) {
+         myThreads[i] = new Thread (new wThreads(dgwNr, sensors[i], busDataMap));
+         myThreads[i].start();
+      }
+   
+   
+      for (int j = 0; j < sensors.length; j++) {
+      
+         try{
+            myThreads[j].join();
+         }
+         catch(InterruptedException e) {
+            busDataMap.put(sensors[j],"Connection to platform was interrupted");
+         }
+      }
+      return busDataMap;
    
    }
 }
--- TestBusInfo.java
+++ TestBusInfo.java
@@ -3,16 +3,9 @@ import java.io.IOException;
 public class TestBusInfo {
 
    public static void main(String[] args) {
-   
-      String a;
-   
-      try {
-         a = BusInfo.getBusInfo("Vin_Num_001", "Online_Users");
-         System.out.println(a);
-      } 
-      catch (IOException e) {
-         System.out.println("IOE");
-      }
+
+      System.out.println(BusInfo.getAllBusInfo("Vin_Num_001").get("At_Stop"));
+      
    }
 
 }
